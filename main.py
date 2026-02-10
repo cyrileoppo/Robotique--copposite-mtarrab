7a4dc590be1c9ef641c6fde43496a9c9a773899c
cyrileoppo <cyrileopposite.pro@gmail.com>
2026-02-10 15:38:32 +0100

    Résolution de l'exercice

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import math
 from robot.robot_mobile import RobotMobile
 
 robot = RobotMobile(0.0, 0.0, 0.0)
 robot.avancer(1.0) 
-robot.avancer(1.0)
+angle_rad = math.radians(45)
+robot.tourner(angle_rad)
+robot.avancer(3.0)
+print("État final du robot :")
 robot.afficher()
+
+"""
+------- RÉSULTAT FINAL -------
+État final du robot :
+(x=3.12, y=2.12, orientation=0.79)
+-------------------------------
+"""
