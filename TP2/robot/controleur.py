7b4b18ce4e7a01981fa22cb4c1489c474b8e1364
cyrileoppo <cyrileopposite.pro@gmail.com>
2026-02-24 14:47:19 +0100

    Fix: recréation du controleur

--- TP2/robot/controleur.py
+++ TP2/robot/controleur.py
@@ -1,5 +1,17 @@
 import pygame
-from .controleur import Controleur
+from abc import ABC, abstractmethod
+
+class Controleur(ABC):
+    @abstractmethod
+    def lire_commande(self): pass
+
+class ControleurTerminal(Controleur):
+    def lire_commande(self):
+        try:
+            entree = input("Commande (v omega) : ").split()
+            return float(entree[0]), float(entree[1])
+        except:
+            return 0.0, 0.0
 
 class ControleurClavierPygame(Controleur):
     def lire_commande(self):
