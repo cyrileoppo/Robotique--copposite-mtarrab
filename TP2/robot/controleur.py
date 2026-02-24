2e48a3d2421873dd6ae698aefdd80e4da81424cd
cyrileoppo <cyrileopposite.pro@gmail.com>
2026-02-24 14:32:42 +0100

    feat: ajout du contrôle clavier via Pygame

--- TP2/robot/controleur.py
+++ TP2/robot/controleur.py
@@ -1,13 +1,12 @@
-from abc import ABC, abstractmethod
+import pygame
+from .controleur import Controleur
 
-class Controleur(ABC):
-    @abstractmethod
-    def lire_commande(self): pass
-
-class ControleurTerminal(Controleur):
+class ControleurClavierPygame(Controleur):
     def lire_commande(self):
-        try:
-            entree = input("Commande (v omega) : ").split()
-            return float(entree[0]), float(entree[1])
-        except:
-            return 0.0, 0.0
+        keys = pygame.key.get_pressed()
+        v, omega = 0.0, 0.0
+        if keys[pygame.K_UP]: v = 2.0
+        if keys[pygame.K_DOWN]: v = -2.0
+        if keys[pygame.K_LEFT]: omega = 1.5
+        if keys[pygame.K_RIGHT]: omega = -1.5
+        return v, omega
